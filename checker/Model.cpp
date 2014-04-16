d89e035fedcf3f1c1543b0e7b0bb24f7592aab10
Daniel Kalmar <kalmard@gmail.com>
2014-04-17 00:01:16 +0200

    Very basic brute force method.

--- checker/Model.cpp
+++ checker/Model.cpp
@@ -1,5 +1,14 @@
 #include "Model.h"
 #include "World.h"
+#include <iostream>
+
+void Model::print() const {
+    for (const auto& it : below) {
+        std::cout << it.first << " -> " << it.second << " ";
+    }
+
+    std::cout << "\n";
+}
 
 bool Model::isComplete() const {
     return below.size() == world.cubes.size();
--- checker/Model.h
+++ checker/Model.h
@@ -6,6 +6,12 @@ class World;
 
 class Model {
 public:
+    Model(const World& world) :
+            world(world) {
+    }
+
+    void print() const;
+
     std::map<Cube, Cube> below;
     bool isComplete() const;
     const World& world;
